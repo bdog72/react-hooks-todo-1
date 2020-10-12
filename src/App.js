74193f4af4577f21dcd07b11957e00c9da319dd9
Brian Bycynski <high5bri@hotmail.com>
2020-10-12 01:14:02 -0400

    Init Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+//
+//
+
+import React from 'react';
+import './App.css';
+
+import { Button } from '@material-ui/core';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Bozo</h1>
+      <Button variant='contained' color='secondary'>
+        Bozo Boy
+      </Button>
+    </div>
+  );
+}
+
+export default App;
